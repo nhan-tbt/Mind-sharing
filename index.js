fe865d889572ba22e704f394f4948a42dc7095f7
nhaatjminh <minhphan.240199@gmail.com>
2021-01-05 01:30:07 +0700

    add upload files function

--- index.js
+++ index.js
@@ -14,47 +14,47 @@ const io = socketio(server);
 
 var chatId = 1;
 
-app.post('/upload', function(req, res) {
-    //Storage
-    // const storage = multer.diskStorage({
-    //     destination: __dirname + '/' + '/images',
-    //     filename: function(req, file, cb) {
-    //         cb(null, 'b' + path.extname(file.originalname))
-    //     }
-    // })
+// app.post('/upload', function(req, res) {
+//     //Storage
+//     // const storage = multer.diskStorage({
+//     //     destination: __dirname + '/' + '/images',
+//     //     filename: function(req, file, cb) {
+//     //         cb(null, 'b' + path.extname(file.originalname))
+//     //     }
+//     // })
 
-    // const upload = multer({
-    //     storage: storage
-    // }).single('myImage');
+//     // const upload = multer({
+//     //     storage: storage
+//     // }).single('myImage');
 
-    const storage = multer.diskStorage({
-        destination: __dirname + '/' + '/images',
-        filename: function(req, file, cb) {
-            cb(null, 'user_' + file.originalname);
-        }
-    })
-    
-    const upload = multer({
-        storage: storage
-    }).array("files", 12);
+//     const storage = multer.diskStorage({
+//         destination: __dirname + '/' + '/images',
+//         filename: function(req, file, cb) {
+//             cb(null, 'user_' + file.originalname);
+//         }
+//     })
 
-    upload(req, res, (err) => {
-        if(err){
-            console.log(err)
-        } else {
-            console.log('uploaded');
-        }
-    });
-});
+//     const upload = multer({
+//         storage: storage
+//     }).array("files", 12);
+
+//     upload(req, res, (err) => {
+//         if(err){
+//             console.log(err)
+//         } else {
+//             console.log('uploaded');
+//         }
+//     });
+// });
 
 app.engine('hbs', hbs({
-    extname:'hbs',
+    extname: 'hbs',
     defaultLayout: 'layout',
-    layoutsDir: __dirname +'/views/layouts',
-    partialsDir: __dirname +'/views/particals',
-    runtimeOptions:{
-      allowProtoPropertiesByDefault: true
-  },
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/particals',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true
+    },
 }));
 
 app.use(express.static(__dirname));
@@ -62,7 +62,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.set('view engine', 'hbs');
-app.set('port',(process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 app.set('currentUser', '');
 
 io.on('connection', socket => {
@@ -86,33 +86,33 @@ io.on('connection', socket => {
     })
 })
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.locals.currentUser = req.app.get('currentUser');
-    
-    controller.searchAllPost(function(posts){
+
+    controller.searchAllPost(function (posts) {
         res.locals.posts = posts;
-        
+
         res.render('index');
     });
 })
 
-app.get('/profile',function(req,res){
+app.get('/profile', function (req, res) {
     res.render('profile');
 })
 
-app.get('/contact',function(req,res){
+app.get('/contact', function (req, res) {
     res.render('contact');
 })
 
-app.get('/create_post',function(req,res){
+app.get('/create_post', function (req, res) {
     res.render('create_post');
 })
 
-app.get('/message',function(req,res){
-    controller.searchChat(req.app.get('currentUser'), function(chats) {
-        controller.searchAcc(chats[0].UserId, function(user){
+app.get('/message', function (req, res) {
+    controller.searchChat(req.app.get('currentUser'), function (chats) {
+        controller.searchAcc(chats[0].UserId, function (user) {
             chatId = chats[0].id;
-            controller.searchMess(chats[0].id,function(messes){
+            controller.searchMess(chats[0].id, function (messes) {
                 res.locals.enemy = user;
                 res.locals.messes = messes;
                 res.locals.chats = chats;
@@ -136,42 +136,42 @@ app.get('/setting-general', function (req, res) {
     res.render('setting-general');
 })
 
-app.get('/test',function(req,res){
+app.get('/test', function (req, res) {
     res.render('test');
 })
 
-app.get('/login',function(req,res){
+app.get('/login', function (req, res) {
     res.locals.layout = 'log_res_Layout.hbs';
 
     res.render('login');
 })
 
-app.get('/user_wall',function(req,res){
+app.get('/user_wall', function (req, res) {
     res.render('user_wall');
 })
 
-app.get('/login',function(req,res){
+app.get('/login', function (req, res) {
     res.locals.layout = 'log_res_Layout.hbs';
 
     res.render('login');
 })
 
 
-app.get('/register',function(req,res){
+app.get('/register', function (req, res) {
     res.locals.layout = 'log_res_Layout.hbs';
 
     res.render('register');
 })
 
 app.post('/get_infor_register', (req, res) => {
-    if (req.body.account.includes(" ")){
+    if (req.body.account.includes(" ")) {
         res.locals.layout = 'log_res_Layout.hbs';
         res.locals.resAnnoun = '*Account cannot contain space';
         res.render('register');
     }
     else {
-        controller.searchAcc(req.body.account, function(this_user) {
-            if (this_user != null){
+        controller.searchAcc(req.body.account, function (this_user) {
+            if (this_user != null) {
                 res.locals.layout = 'log_res_Layout.hbs';
                 res.locals.resAnnoun = '*Account ' + req.body.account + ' has already exists';
 
@@ -197,10 +197,10 @@ app.post('/get_infor_register', (req, res) => {
                     nation: "",
                     bio: "",
                 }
-    
+
                 req.app.set('currentUser', req.body.account);
                 user_name = req.body.account;
-    
+
                 controller.createAcc(userAcc);
                 res.redirect("/");
             }
@@ -210,9 +210,9 @@ app.post('/get_infor_register', (req, res) => {
 
 app.post('/get_infor_login', (req, res) => {
 
-    controller.searchAcc(req.body.account, function(this_user) {
-        if (this_user != null){
-            if (bcrypt.compareSync(req.body.password, this_user.password)){
+    controller.searchAcc(req.body.account, function (this_user) {
+        if (this_user != null) {
+            if (bcrypt.compareSync(req.body.password, this_user.password)) {
                 req.app.set('currentUser', req.body.account);
 
                 res.redirect("/");
@@ -246,7 +246,7 @@ app.post('/get_infor_login', (req, res) => {
 //             }
 //             else {
 //                 res.render('setting-password', { Announ: '*Password does not match'});
-                
+
 //             }
 //         }
 //         else {
@@ -255,6 +255,53 @@ app.post('/get_infor_login', (req, res) => {
 //     })
 // })
 
-server.listen(app.get('port'),function(){
-    console.log("Server is listening on port "+ app.get('port'))
+server.listen(app.get('port'), function () {
+    console.log("Server is listening on port " + app.get('port'))
 });
+
+
+const AWS = require('aws-sdk');
+const Busboy = require('busboy');
+
+const BUCKET_NAME = 'mind-sharing';
+const IAM_USER_KEY = 'AKIATRGWWZO2WUH5TFNJ';
+const IAM_USER_SECRET = '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';
+
+const storage = multer.memoryStorage({
+    destination: function(req, file, callback) {
+        callback(null, '')
+    },
+})
+
+const upload = multer({storage}).array("files", 3);
+
+app.post('/upload', upload, function (req, res, next) {
+
+    var files = req.files;
+
+    let s3bucket = new AWS.S3({
+        accessKeyId: IAM_USER_KEY,
+        secretAccessKey: IAM_USER_SECRET,
+        Bucket: BUCKET_NAME
+    });
+    for (let i = 0; i < files.length; i++) {
+        var file = files[i];
+        console.log(file);
+
+        s3bucket.createBucket(function () {
+            var params = {
+                Bucket: BUCKET_NAME,
+                Key: file.originalname,
+                Body: file.buffer
+            };
+            s3bucket.upload(params, function (err, data) {
+                if (err) {
+                    console.log('error in callback');
+                    console.log(err);
+                }
+                console.log('success');
+                console.log(data);
+            });
+        });
+    }
+}) 
--- js/create_post.js
+++ js/create_post.js
@@ -1,22 +1,46 @@
 const file = document.getElementById("file");
 const previewContainer = document.getElementById("imgPreview");
-const previewImage = previewContainer.querySelector(".image-preview");
+const inner = document.getElementById("inner");
+const indicators = document.getElementById("indicators");
+var act = "";
 
 file.addEventListener("change", function() {
-    const f = this.files[0];
-
+    const f = this.files;
+    indicators.innerHTML = '';
+    inner.innerHTML = '';
     if (f) {
-        const reader = new FileReader();
-        previewImage.style.display = "block";
-        
-        reader.addEventListener("load", function() {
-            previewImage.setAttribute("src", this.result);
-        })
+        for (let i = 0; i < f.length; i++) {
+            var reader = new FileReader();
+            
+            
+            console.log(act);
 
-        reader.readAsDataURL(f);
+            
+            reader.addEventListener("load", function(event) {
+                var picFile = event.target;
+                if (i === 0) {
+                    act = "active";
+                }
+                else {
+                    act = "";
+                }
+                var indi_htm = `<li data-target="#carouselId" data-slide-to="` + i + `" class="` + act + `"></li>`;
+                var inner_htm = `<div class="carousel-item ` + act + `">
+                                    <center><img src="` + picFile.result + `" alt="" class="image-preview"></center>
+                                </div>`;
+                indicators.insertAdjacentHTML("beforeend", indi_htm);
+                inner.insertAdjacentHTML("beforeend", inner_htm);
+            })
+            reader.readAsDataURL(f[i]);
+        } 
     }
     else {
-        previewImage.display = null;
-        previewImage.setAttribute("src", "");
+        //previewImage.display = null;
+        //previewImage.setAttribute("src", "");
+        console.log(1);
+        while(previewContainer.hasChildNodes()) {
+            previewContainer.removeChild(previewContainer.childNodes[0]);
+        }
+        
     }
 })
